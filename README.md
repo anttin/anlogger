af12ba2371fe540e58892bb431c21b70e2280377
Antti Nilsson <muut.py@antion.fi>
2023-01-22 19:52:08 +0200

    Add option to redirect or disable console logging

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ python3 -m pip install anlogger
 
 ```python
 from anlogger import Logger
-logger_obj = Logger(name="appname", default_loglevel="INFO", fmt=None, syslog=None)
+logger_obj = Logger(name="appname", default_loglevel="INFO", fmt=None, syslog=None, syslog_facility=None, log_to_console=True, console_stream='stderr')
 logger = logger_obj.get()
 logger.info("Message on info-level")
 ```
@@ -24,5 +24,11 @@ logger.info("Message on info-level")
 `fmt` is the format used for formatting the logger. Se python's logging module documentation for formattion options.
 
 `syslog` is the syslog configuration. Set to `True` to use local syslog, or a tuple of `("ipaddress-string", port-int)` for remote logging.
+
+`syslog_facility` is one of well-known syslog facilities. If syslog is used but `syslog_facility` is not set, the `user` facility is used by default.
+
+`log_to_console` defines whether the logging is also outputted to the console. Default is `True`.
+
+`console_stream` defines which output stream to use for console logging. Accepted values are `stderr` (default) and `stdout`.
 
 See `logger.Logger` class code for additional details.
--- anlogger/logger.py
+++ anlogger/logger.py
@@ -1,23 +1,46 @@
 import logging
 import logging.handlers
 import os
+import sys
 
 class Logger(object):
 
-  def __init__(self, name, default_loglevel='INFO', fmt=None, syslog=None, syslog_facility=None):
+  def __init__(self,
+               name,
+               default_loglevel='INFO',
+               fmt=None,
+               syslog=None,
+               syslog_facility=None,
+               log_to_console=True,
+               console_stream='stderr'):
+
     self.name   = name
     self.syslog = syslog
     self.syslog_facility = syslog_facility
     self.fmt = fmt if fmt is not None else "%(asctime)-15s %(name)s %(levelname)s %(message)s"
+    self.log_to_console = log_to_console
 
     if 'LOGLEVEL' in os.environ:
       self.level = os.environ['LOGLEVEL'].upper()
     else:
       self.level = default_loglevel.upper()
 
-    logging.basicConfig(format=self.fmt)
     self.logger = logging.getLogger(self.name)
     self.logger.setLevel(self.level)
+
+    formatter = logging.Formatter(fmt=self.fmt)
+
+    if self.log_to_console:
+      if console_stream == 'stderr':
+        console_stream = sys.stderr
+      elif console_stream == 'stdout':
+        console_stream = sys.stdout
+      else:
+        raise ValueError(f'Unknown value for console_stream: {console_stream}')
+
+      consoleHandler = logging.StreamHandler(stream=console_stream)
+      consoleHandler.setFormatter(formatter)
+      self.logger.addHandler(consoleHandler)
 
     if self.syslog is not None and self.syslog not in (False, 0):
       if isinstance(self.syslog, (list, tuple)):
@@ -28,11 +51,12 @@ class Logger(object):
         _addr = "/dev/log" if os.path.exists("/dev/log") else None
 
       if _addr is not None:
-        handler = logging.handlers.SysLogHandler(
+        syslogHandler = logging.handlers.SysLogHandler(
           address=_addr,
           facility=syslog_facility
         )
-        self.logger.addHandler(handler)
+        syslogHandler.setFormatter(formatter)
+        self.logger.addHandler(syslogHandler)
 
 
   def get(self):
